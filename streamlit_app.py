315b64098fe735b720311cb65e90add26e052458
kasperfernhout01 <108528854+kasperfernhout01@users.noreply.github.com>
2022-07-20 11:14:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-streamlit.header("Fruityvice Fruit Advice!")
-
 # import requests
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
@@ -34,6 +32,8 @@ streamlit.write('The user entered ', fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
+streamlit.header("Fruityvice Fruit Advice!")
 
 # create normalized data
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
