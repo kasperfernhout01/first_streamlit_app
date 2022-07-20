84ded90369434873be0ea1413c11d5dd324d0141
kasperfernhout01 <108528854+kasperfernhout01@users.noreply.github.com>
2022-07-20 11:01:17 +0200

    Update streamlit_app.py
    write to dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# create normalized data
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write to dataframe
+streamlit.dataframe(fruityvice_normalized)
